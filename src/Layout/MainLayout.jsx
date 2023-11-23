de85fcd45b39f80c563213e80138c9f800519033
Mahmud Munna <smbmunna@gmail.com>
2023-11-24 00:32:54 +0600

    first commit

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "../Components/Navbar/Navbar";
+import Footer from "../Components/Footer/Footer";
+
+
+const MainLayout = () => {
+    return (
+        <div>
+            <Navbar/>
+            <Outlet/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default MainLayout;
